8ef65968d60cae58312bfc2ea707f3e0f86a2d5a
Ethan Lew <elew@galois.com>
2023-05-26 21:21:02 -0700

    comment and captialize

--- api/index.js
+++ api/index.js
@@ -1,33 +1,50 @@
+// Import the required libraries
 const express = require('express');
 const axios = require('axios');
+
+// Initialize an Express application
 const app = express();
 
-async function getRating(username, format) {
-  const url = `https://lichess.org/api/user/${username}`;
-  const response = await axios.get(url);
-  return response.data.perfs[format].rating;
-}
+// Set up a route for GET requests
+app.get('/', async (req, res) => {
 
-app.get('/api', async (req, res) => {
-  const { username, format } = req.query;
+    // Retrieve the username and format from the query parameters
+    const { username, format } = req.query;
 
-  if (!username || !format) {
-    res.send("Invalid request. You need to provide a Lichess username and format.");
-    return;
-  }
+    // Capitalize the format
+    const capitalizedFormat = format.charAt(0).toUpperCase() + format.slice(1);
 
-  try {
-    const rating = await getRating(username, format);
-    const badgeUrl = `https://img.shields.io/badge/Lichess%20${format}-${rating}-blue.svg`;
-    const svgResponse = await axios.get(badgeUrl, { responseType: 'arraybuffer' });
+    // Call the Lichess API to retrieve the user's ratings
+    let user;
+    try {
+        const response = await axios.get(`https://lichess.org/api/user/${username}`);
+        user = response.data;
+    } catch (error) {
+        // If the Lichess API call fails, return an error response
+        return res.status(500).send('Error fetching data from Lichess API');
+    }
 
-    res.setHeader('Cache-Control', 'no-store, must-revalidate');
+    // Extract the desired rating from the user's ratings
+    const rating = user.perfs[format].rating;
+
+    // Create the URL for the shields.io API to generate the shield
+    const shieldUrl = `https://img.shields.io/badge/Lichess%20${capitalizedFormat}-${rating}-blue`;
+
+    // Call the shields.io API to retrieve the shield SVG
+    let svg;
+    try {
+        const response = await axios.get(shieldUrl);
+        svg = response.data;
+    } catch (error) {
+        // If the shields.io API call fails, return an error response
+        return res.status(500).send('Error fetching shield from Shields.io');
+    }
+
+    // Return the shield SVG with the appropriate content type
     res.setHeader('Content-Type', 'image/svg+xml');
-    res.send(svgResponse.data);
-  } catch (e) {
-    res.send(e.message);
-  }
+    res.send(svg);
 });
 
+// Export the app as a Vercel Serverless Function
 module.exports = app;
 
